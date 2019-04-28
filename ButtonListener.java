e50adf1cd9cbeb90ddc3d024260b0b12c462294e
goldfishinsky <33257511+goldfishinsky@users.noreply.github.com>
2019-04-28 23:14:24 +0800

    Add files via upload

---
+++ ButtonListener.java
@@ -0,0 +1,53 @@
+package wuziqi0002;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JComboBox;
+
+public class ButtonListener implements ActionListener{
+	playListener p;
+	JComboBox<String> box;
+	public ButtonListener(playListener p,JComboBox<String> box) {
+		// TODO Auto-generated constructor stub
+		this.p = p;
+		this.box =box;
+	}
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		System.out.println(e.getActionCommand());
+		switch (e.getActionCommand()) {
+		case "新游戏":
+			p.replay();
+			break;
+		case "悔棋":
+			p.regret();
+			break;
+		case "认输":
+			p.defeat();
+			break;
+		default:
+			System.out.println("你按了啥");
+			break;
+		}
+		switch (box.getSelectedIndex()) {
+			//人机对战
+			case 0:
+				p.battleType = 0;
+				break;
+			//人人对战
+			case 1:
+				p.battleType = 1;
+				break;
+			//机机对战
+			case 2:
+				p.battleType = 2;
+				break;
+			default:
+				System.out.println("你选了什么。。。");
+				break;
+		}
+	}
+
+}
---
+++ ChessPosition.java
@@ -0,0 +1,13 @@
+package wuziqi0002;
+//记录棋子历史位置的类
+public class ChessPosition {
+	public int xi,yi;//横纵坐标
+	public ChessPosition(int xi, int yi) {
+		// TODO Auto-generated constructor stub
+		this.xi = xi;
+		this.yi = yi;
+	}
+	public ChessPosition() {
+		// TODO Auto-generated constructor stub
+	}
+}
